b0d019bf4dc62b56a76bfc7525a20d4eeff1852b
Andrew Obiano <andrew.obiano@gmail.com>
2020-02-03 04:20:32 -1000

    added comments

--- compatibilityPredictor.js
+++ compatibilityPredictor.js
@@ -1,9 +1,8 @@
 const data = require("./input");
 const applicants = data.input().applicants;
 
-// Attribute weight calculations on Google Spreadsheet
-// https://docs.google.com/spreadsheets/d/1PhgRtenUvESLeiJEHZSPalS9fsJnHyIv7a9z8TLHAQo/edit?usp=sharing
-
+// Attribute weightings based on Decision Making Matrix (DMM) linked below
+// https://docs.google.com/spreadsheets/d/1PhgRtenUvESLeiJEHZSPalS9fsJnHyIv7a9z8TLHAQo/edit?usp=
 const attWeight = {
   intWeight: 0.2,
   strWeight: 0.05,
@@ -11,10 +10,12 @@ const attWeight = {
   spiWeight: 0.7
 };
 
+// Output answer
 let output = {
   scoredApplicants: []
 };
 
+// Loop through the attributes for each app and calculate total scores
 for (let person in applicants) {
   let intSubtotal =
     applicants[person].attributes.intelligence * attWeight.intWeight;
@@ -29,24 +30,35 @@ for (let person in applicants) {
 
   output.scoredApplicants.push({
     name: applicants[person].name,
-    score: Math.round((total / 9) * 10) / 10
+    score: Math.round((total / 9) * 10) / 10 // Score calculation on DMM linked above
   });
 }
 
-console.log("Here are your results! ==============================");
-console.log(output);
-console.log("Here are your results! ==============================");
+setTimeout(function() {
+  // Set timeout to prevent log from interfering with test logs
+  console.log("Here are your results!");
+  console.log(output);
+}, 50);
 
+// Function to check for best applicant once/if test is run
 exports.compatibilityPredictor = function() {
   let lowestScore = 1;
   let compatible = null;
 
+  // Loop through total scores and find the lowest value
   for (let person in output.scoredApplicants) {
     if (output.scoredApplicants[person].score < lowestScore) {
       lowestScore = output.scoredApplicants[person].score;
       compatible = output.scoredApplicants[person].name;
     }
   }
+
+  // Set timeout to prevent log from interfering with test logs
+  setTimeout(function() {
+    console.log(
+      `Based on the results, your most compatible applicant is ${compatible}`
+    );
+  }, 50);
 
   return compatible;
 };
--- test/compatibilityPredictor_spec.js
+++ test/compatibilityPredictor_spec.js
@@ -2,6 +2,7 @@ const predictorModule = require("../compatibilityPredictor");
 const chai = require("chai");
 const should = chai.should();
 
+// Test check for sample data provided
 describe("The most compatible applicant", function() {
   it("should be the applicant with the lowest score", function() {
     let predictor = predictorModule.compatibilityPredictor();
